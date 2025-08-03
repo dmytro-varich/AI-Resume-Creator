a0f834b51ee8542d9c8c0a147633ffbd55728d2d
ywindows98 <tkachenko.iwn@gmail.com>
2025-08-03 21:03:58 +0200

    ResumeCreatorBackend: AccountController: Write tests for invalid model check. Invalid Name, Email or Password have to return BadRequest.

--- ResumeCreatorBackend/ResumeCreatorBackend.Tests/AccountControllerTests.cs
+++ ResumeCreatorBackend/ResumeCreatorBackend.Tests/AccountControllerTests.cs
@@ -20,7 +20,60 @@ namespace ResumeCreatorBackend.Tests
 
             var badRequest = Assert.IsType<BadRequestObjectResult>(result);
             Assert.Equal("Account data is required.", badRequest.Value);
-            
+        }
+
+        [Fact]
+        public async Task CreateAccountAsync_InvalidName_ReturnsBadRequest()
+        {
+            var createAccountTempModel = new CreateAccountTemp();
+
+            var mockAccountService = new Mock<IAccountService>();
+            var accountController = new AccountController(mockAccountService.Object);
+
+            accountController.ModelState.AddModelError("Name", "Name is invalid.");
+
+            var result = await accountController.CreateAccountAsync(createAccountTempModel);
+
+            var badRequest = Assert.IsType<BadRequestObjectResult>(result);
+            var returnedModelState = Assert.IsType<SerializableError>(badRequest.Value);
+
+            Assert.True(returnedModelState.ContainsKey("Name"));
+        }
+
+        [Fact]
+        public async Task CreateAccountAsync_InvalidEmail_ReturnsBadRequest()
+        {
+            var createAccountTempModel = new CreateAccountTemp();
+
+            var mockAccountService = new Mock<IAccountService>();
+            var accountController = new AccountController(mockAccountService.Object);
+
+            accountController.ModelState.AddModelError("Email", "Email is invalid.");
+
+            var result = await accountController.CreateAccountAsync(createAccountTempModel);
+
+            var badRequest = Assert.IsType<BadRequestObjectResult>(result);
+            var returnedModelState = Assert.IsType<SerializableError>(badRequest.Value);
+
+            Assert.True(returnedModelState.ContainsKey("Email"));
+        }
+
+        [Fact]
+        public async Task CreateAccountAsync_InvalidPassword_ReturnsBadRequest()
+        {
+            var createAccountTempModel = new CreateAccountTemp();
+
+            var mockAccountService = new Mock<IAccountService>();
+            var accountController = new AccountController(mockAccountService.Object);
+
+            accountController.ModelState.AddModelError("Password", "Password is invalid.");
+
+            var result = await accountController.CreateAccountAsync(createAccountTempModel);
+
+            var badRequest = Assert.IsType<BadRequestObjectResult>(result);
+            var returnedModelState = Assert.IsType<SerializableError>(badRequest.Value);
+
+            Assert.True(returnedModelState.ContainsKey("Password"));
         }
     }
 }
