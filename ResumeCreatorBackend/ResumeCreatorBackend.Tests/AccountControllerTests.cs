737956cfca3bae0a4125b9750b07a25dc1094885
ywindows98 <tkachenko.iwn@gmail.com>
2025-08-03 22:00:21 +0200

    ResumeCreatorBackend: AccountController: Write test for successful account creation which have to return Ok response.

--- ResumeCreatorBackend/ResumeCreatorBackend.Tests/AccountControllerTests.cs
+++ ResumeCreatorBackend/ResumeCreatorBackend.Tests/AccountControllerTests.cs
@@ -5,6 +5,7 @@ using ResumeCreatorBackend.Services;
 using ResumeCreatorBackend.Controllers;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using ResumeCreatorBackend.Models;
 
 namespace ResumeCreatorBackend.Tests
 {
@@ -97,6 +98,31 @@ namespace ResumeCreatorBackend.Tests
 
             var badRequest = Assert.IsType<BadRequestObjectResult>(result);
             Assert.Equal("Account with the given email already exists.", badRequest.Value);
+        }
+
+        [Fact]
+        public async Task CreateAccountAsync_SuccessfulCreation_ReturnsOk()
+        {
+            var createAccountTempModel = new CreateAccountTemp()
+            {
+                Name = "John12",
+                Email = "johny12@gmail.com",
+                Password = "password"
+            };
+
+            var resultingAccountModel = new AccountModel();
+
+            var mockAccountService = new Mock<IAccountService>();
+            mockAccountService
+                .Setup(s => s.CreateAccountAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+                .ReturnsAsync(resultingAccountModel);
+
+            var accountController = new AccountController(mockAccountService.Object);
+
+            var result = await accountController.CreateAccountAsync(createAccountTempModel);
+
+            var okResponse = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal("Account created successfully.", okResponse.Value);
         }
     }
 }
